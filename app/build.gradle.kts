f845526c29bf10e5e135f11cc5a8f389772e7c94
selimdawa <selimdawa@gmail.com>
2026-06-09 03:25:16 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,13 @@ plugins {
 android {
     namespace = "com.littleapp.blogger"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+       version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.blogger"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
