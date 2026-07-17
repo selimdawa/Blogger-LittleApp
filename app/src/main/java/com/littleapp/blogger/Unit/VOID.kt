ccdff71ef99e57be6ec4eaca579a45526db8db51
selimdawa <selimdawa@gmail.com>
2026-07-17 06:16:51 +0300

    Coil - Add

--- app/src/main/java/com/littleapp/blogger/Unit/VOID.kt
+++ app/src/main/java/com/littleapp/blogger/Unit/VOID.kt
@@ -3,7 +3,7 @@ package com.littleapp.blogger.unit
 import android.content.Context
 import android.content.Intent
 import android.widget.ImageView
-import com.bumptech.glide.Glide
+import coil.load
 import com.littleapp.blogger.R
 
 object VOID {
@@ -21,9 +21,13 @@ object VOID {
     fun loadImage(context: Context?, url: String?, imageView: ImageView) {
         try {
             if (context != null) {
-                Glide.with(context).load(url).placeholder(R.color.image_profile).into(imageView)
+                imageView.load(url) {
+                    placeholder(R.color.image_profile)
+                    error(R.color.image_profile)
+                    crossfade(true)
+                }
             }
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             imageView.setImageResource(R.color.image_profile)
         }
     }
